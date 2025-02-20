7468b39a039ce9fa8719c738337fc208b9e0af08
Abdoulkarim <17a.karim@gmail.com>
2025-02-20 11:37:16 +0000

    completed form wave

---
+++ form-wave/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const labels = document.querySelectorAll("label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.textContent
+    .split("")
+    .map(
+      (letter, i) =>
+        `<span style="transition-delay: ${i * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
