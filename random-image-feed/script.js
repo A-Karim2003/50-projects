326d93d7f59d9c260a90a5eaa786b870da1f361e
Abdoulkarim <17a.karim@gmail.com>
2025-03-23 14:49:19 +0000

    improved random image feed by making it more interactive

--- random-image-feed/script.js
+++ random-image-feed/script.js
@@ -2,11 +2,23 @@
 const randomImgSrc = "https://picsum.photos/300/300";
 
 const container = document.querySelector(".grid-container");
+const amountEl = document.getElementById("amount");
 
-for (let i = 0; i < 21; i++) {
-  const div = document.createElement("div");
-  const img = document.createElement("img");
-  img.src = `${randomImgSrc}?random=${i}`;
-  div.append(img);
-  container.append(div);
+let amount = 15;
+
+displayImgs(); // display inital images
+amountEl.addEventListener("change", () => {
+  amount = amountEl.value;
+  container.textContent = "";
+  displayImgs();
+});
+
+function displayImgs() {
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    img.src = `${randomImgSrc}?random=${i}`;
+    div.append(img);
+    container.append(div);
+  }
 }
